f554c49ac5b7b5b4b774b1b90d60b3d83eb7aa0c
MenSolomon <ekmensolomon@gmail.com>
2023-09-26 02:15:47 +0100

    Added Pitch image to bio page for the first time

--- src/screens/subscreens/PlayerBio.jsx
+++ src/screens/subscreens/PlayerBio.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { Card } from "@mui/material";
+import pitch from "../../assets/images/pitch.jpg";
 
 const PlayerBio = () => {
   return (
-    <div style={{ width: "100%", height: "42vh", background: "teal" }}>
-      PlayerBio
+    <div
+      style={{
+        width: "100%",
+        height: "40vh",
+        // background: "teal",
+        display: "flex",
+      }}
+    >
+      <div style={{ flex: ".18", padding: "0vw" }}>
+        <Card
+          sx={{
+            width: "100%",
+            height: "100%",
+            backgroundImage: `url(${pitch})`,
+            backgroundSize: "contain",
+          }}
+        ></Card>
+      </div>
     </div>
   );
 };
