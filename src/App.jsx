02b4e3786b9cc3765a77b30018c9cd263e93967b
MenSolomon <ekmensolomon@gmail.com>
2024-03-13 01:11:58 +0000

    no change occured

--- src/App.jsx
+++ src/App.jsx
@@ -434,10 +434,12 @@ const App = () => {
               variant="contained"
               color="success"
               size="small"
-              onClick={() => navigate("/changeSubscription")}>
+              onClick={() => navigate("/changeSubscription")}
+            >
               Get One
             </Button>
-          }>
+          }
+        >
           No or Inactive Subscrtiption
         </Alert>
       )}
@@ -500,7 +502,8 @@ const App = () => {
           {/* COACH AGENT AND SCOUT VERSION */}
           <Route
             path="/multiStudio"
-            element={<CoachAgentScoutVersionMotherComponent />}>
+            element={<CoachAgentScoutVersionMotherComponent />}
+          >
             <Route path="/multiStudio/favorite" element={<Error404 />} />
             <Route path="/multiStudio/help" element={<Error404 />} />
             <Route path="/multiStudio/settings" element={<Settings />} />
